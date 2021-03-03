790afb83a9554f2494a2ee1008c448c88cabc9d9
DESKTOP-OO8H1Q9\jakub <smolik.ja@protonmail.com>
2021-03-03 14:03:40 +0100

    buggy navigation

--- lib/main.dart
+++ lib/main.dart
@@ -3,54 +3,35 @@ import 'package:bp_flutter_app/models/characters_model.dart';
 import 'package:bp_flutter_app/models/movies_model.dart';
 import 'package:bp_flutter_app/models/quotes_model.dart';
 import 'package:bp_flutter_app/helpers/json_parse_helper.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:bp_flutter_app/services/app_localizations.dart';
+import 'package:bp_flutter_app/widgets/phoenix.dart';
 
-void main() {
-  runApp(MyApp());
+class _BottomBarItem {
+  const _BottomBarItem(this.index, this.titleKey, this.icon, this.iconActive);
+
+  final int index;
+  final String titleKey;
+  final Widget icon;
+  final Widget iconActive;
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+List<_BottomBarItem> _bottomBarItems = <_BottomBarItem>[
+  _BottomBarItem(0, 'appbar_guess_quote', Icon(Icons.article_outlined), null),
+  _BottomBarItem(1, 'appbar_characters', Icon(Icons.date_range_outlined), null)
+];
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  MyHomePage({Key key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<GlobalKey<NavigatorState>> _navigatorKeys =
+      List<GlobalKey<NavigatorState>>.generate(_bottomBarItems.length, (index) => GlobalKey<NavigatorState>()).toList();
   int _counter = 0;
+  int _currentIndex = 0;
 
   CharactersModel charactersData;
   MoviesModel moviesData;
@@ -82,34 +63,45 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('LOTR Guess the Quote'),
       ),
       body: Center(
           child: FutureBuilder(
         future: Future.wait([_charactersFuture(), _moviesFuture(), _quotesFuture()]),
         builder: (context, snapshot) {
-          if (snapshot.data != null) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
             charactersData = JsonParseHelper().getCharacters(snapshot.data[0]);
             moviesData = JsonParseHelper().getMovies(snapshot.data[1]);
             quotesData = JsonParseHelper().getQuotes(snapshot.data[2]);
           }
 
-          return !isEmpty(charactersData, moviesData, quotesData)
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'You have pushed the button this many times:',
-                    ),
-                    Text(
-                      '$_counter',
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                  ],
-                )
-              : Center(
-                  child: CircularProgressIndicator(),
+          return Scaffold(
+            body: SafeArea(
+              top: false,
+              child: _getContent(),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: _currentIndex,
+              onTap: (int index) {
+                _navigatorKeys[_currentIndex].currentState.popUntil((route) => route.isFirst);
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              items: _bottomBarItems.map((_BottomBarItem item) {
+                return BottomNavigationBarItem(
+                  icon: item.icon,
+                  activeIcon: item.iconActive,
+                  label: AppLocalizations.of(context).translate(item.titleKey),
                 );
+              }).toList(),
+            ),
+          );
         },
       )),
       floatingActionButton: FloatingActionButton(
@@ -127,4 +119,79 @@ class _MyHomePageState extends State<MyHomePage> {
     }
     return isEmpty;
   }
+
+  Widget _getContent() {
+    var routeBuilders = _routeBuilders(_currentIndex);
+    return Navigator(
+      key: _navigatorKeys[_currentIndex],
+      onGenerateRoute: (routeSettings) {
+        return MaterialPageRoute(
+          builder: (context) => routeBuilders[routeSettings.name](context),
+        );
+      },
+    );
+  }
+
+  Map<String, WidgetBuilder> _routeBuilders(int index) {
+    return {
+      '/': (context) {
+        Widget screen;
+        switch (index) {
+          case 0:
+            screen = Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            );
+            break;
+          case 1:
+            screen = Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            );
+            break;
+        }
+        return screen;
+      },
+    };
+  }
+
+  void _fullscreenPush(Widget screen) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
+  }
+}
+
+void main() {
+  runApp(Phoenix(
+    child: MaterialApp(
+      supportedLocales: [Locale('en', 'US')],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      title: 'LOTR Guess Quote',
+      theme: ThemeData(
+        // primarySwatch: createMaterialColor(kPrimaryColor),
+        // primaryTextTheme: TextTheme(headline6: TextStyle(color: createMaterialColor(kPrimaryColor))),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    ),
+  ));
 }
---
+++ lib/widgets/phoenix.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Phoenix extends StatefulWidget {
+  Phoenix({this.child});
+
+  final Widget child;
+
+  static void rebirth(BuildContext context) {
+    context.findAncestorStateOfType<_PhoenixState>().rebirth();
+  }
+
+  @override
+  _PhoenixState createState() => _PhoenixState();
+}
+
+class _PhoenixState extends State<Phoenix> {
+  Key key = UniqueKey();
+
+  void rebirth() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: KeyedSubtree(
+        key: key,
+        child: widget.child,
+      ),
+    );
+  }
+}
