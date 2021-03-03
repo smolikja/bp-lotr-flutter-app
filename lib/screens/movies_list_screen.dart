1eb812f1480d33bf35c873f64851e52085e8577f
DESKTOP-OO8H1Q9\jakub <smolik.ja@protonmail.com>
2021-03-03 22:15:54 +0100

    movie list screen

--- lib/screens/movies_list_screen.dart
+++ lib/screens/movies_list_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:bp_flutter_app/screens/base_stateful_widget.dart';
 import 'package:bp_flutter_app/services/app_localizations.dart';
 import 'package:bp_flutter_app/widgets/custom_appbar.dart';
+import 'package:bp_flutter_app/widgets/list_divider.dart';
+import 'package:bp_flutter_app/models/movies_model.dart';
+import 'package:bp_flutter_app/globals.dart';
+import 'package:bp_flutter_app/helpers/constants.dart';
 
 class MoviesListScreen extends BaseStatefulWidget {
   MoviesListScreen({
@@ -14,6 +18,14 @@ class MoviesListScreen extends BaseStatefulWidget {
 }
 
 class _MoviesListScreenState extends State<MoviesListScreen> {
+  MoviesModel _moviesData;
+
+  @override
+  void initState() {
+    super.initState();
+    _moviesData = globalMovies;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +33,51 @@ class _MoviesListScreenState extends State<MoviesListScreen> {
         title: AppLocalizations.of(context).translate('appbar_movies_list'),
         fullscreenPush: widget.fullscreenPush,
       ),
-      body: SingleChildScrollView(child: Text("Movies list")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.separated(
+              shrinkWrap: true,
+              // physics: NeverScrollableScrollPhysics(),
+              separatorBuilder: (context, index) => ListDivider(indent: 16.0),
+              itemCount: _moviesData.docs.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: ListTile(
+                    onTap: () {
+                      // _onItemTap(items[index].fullscreen, items[index].screen);
+                    },
+                    title: Text(_moviesData.docs[index].name,
+                        style: TextStyle(color: kPrimaryColor, fontSize: 14.0, fontWeight: FontWeight.bold)),
+                    // leading: items[index].icon,
+                    trailing: Icon(Icons.keyboard_arrow_right, color: kGreyLightColor, size: 24),
+                    dense: true,
+                  ),
+                );
+              },
+            ),
+            // FutureBuilder<String>(
+            //     future: _versionFuture,
+            //     builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            //       if (snapshot.connectionState == ConnectionState.done && !snapshot.hasError && snapshot.data != null) {
+            //         return GestureDetector(
+            //           child: Container(
+            //             alignment: Alignment.center,
+            //             width: double.infinity,
+            //             padding: EdgeInsets.only(top: 8, bottom: 16),
+            //             child: Text(
+            //               snapshot.data,
+            //               style: TextStyle(color: kGreyLightColor, fontSize: 14),
+            //             ),
+            //           ),
+            //           onDoubleTap: _switchTestTopicSubscription,
+            //         );
+            //       }
+            //       return Container();
+            //     }),
+          ],
+        ),
+      ),
     );
   }
 }
